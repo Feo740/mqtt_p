bd7e1bff9c8e45b12f9f1b8ef950e7d6797acd6b
Feo740 <feo2006@yandex.ru>
2017-09-02 21:56:53 +0300

    second

--- lib/mqtt_p.ex
+++ lib/mqtt_p.ex
@@ -1,2 +1,13 @@
 defmodule MqttP do
+  def funk() do
+    MMQL.connect("C")
+    MMQL.sub("C","/ESP/flap1")
+    receive do
+      {:subscribed_publish,conn_name,topic,msg}->
+        IO.puts("msg received #{msg}.")
+      after
+        2_000->
+          throw("msg not received")
+    end
+  end
 end
--- mix.exs
+++ mix.exs
@@ -7,14 +7,14 @@ defmodule MqttP.Mixfile do
      elixir: "~> 1.1-dev",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps()]
   end
 
   # Configuration for the OTP application
-  #
+  #n   mn
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger,:mmql]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +27,7 @@ defmodule MqttP.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    []
+    #[]
+  [{:mmql, git: "https://github.com/UA3MQJ/elx-mmql.git"}]
   end
 end
